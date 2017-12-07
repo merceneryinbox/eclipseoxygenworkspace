2b6f582eec0a3fdd24957ee0db49d43731b36488
mercenery <mercenery@inbox.ru>
2017-12-07 12:11:31 +0300

    work

--- BlochEquality/src/equals/IdealBlochEquals.java
+++ BlochEquality/src/equals/IdealBlochEquals.java
@@ -2,6 +2,7 @@ package equals;
 
 import checkers.javari.quals.ReadOnly;
 
+<<<<<<< HEAD
 public class IdealBlochEquals {
     public int fieldInt = 10;
     public String fieldString = "StringField";
@@ -35,4 +36,39 @@ public class IdealBlochEquals {
 
 	return super.toString();
     }
+=======
+class IdealBlochEquals {
+	int fieldInt = 10;
+	String fieldString = "StringField";
+
+	@Override
+	public boolean equals(@ReadOnly final Object anotherObj) {
+
+		if (this != anotherObj) {
+			return anotherObj == null ? false : this == anotherObj;
+		}
+
+		if (!(anotherObj instanceof IdealBlochEquals)) {
+			return false;
+		}
+
+		return (this.fieldInt == 0 ? (((IdealBlochEquals) anotherObj).fieldInt == 0)
+				: (this.fieldInt == ((IdealBlochEquals) anotherObj).fieldInt)
+						&& (this.fieldString.equals(((IdealBlochEquals) anotherObj).fieldString))
+						&& super.equals(anotherObj));
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = (result * 31) + this.fieldInt + this.fieldString.hashCode();
+		return result;
+	}
+
+	@Override
+	public String toString() {
+
+		return super.toString();
+	}
+>>>>>>> work
 }
